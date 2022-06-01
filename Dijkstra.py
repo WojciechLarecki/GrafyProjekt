47a1873fdcb5564752c03e4c68989ac031205a7e
Wojciech <s205838@sggw.edu.pl>
2022-06-02 00:12:14 +0200

    Make output UI and add conversion from text file

---
+++ Dijkstra.py
@@ -0,0 +1,89 @@
+import numpy as np
+from queue import PriorityQueue
+
+
+def Dijkstra(start, stop, G):
+    dist = [float('inf')]*len(G)
+    # -1 oznacza ze brak poprzednika
+    prev = [-1]*len(G)
+    visited = [0]*len(G)
+    # wierzcholki liczymy od zera
+    dist[start] = 0
+    prev[start] = -1
+    H = PriorityQueue()
+    H.put((0, start))
+    while not H.empty():
+        (u_dist, u) = H.get()
+        # usuwam elementy ktore juz byly odwiedzone w kolejce
+        while not H.empty():
+            if visited[u] == 0:
+                break
+            (u_dist, u) = H.get()
+        visited[u] = 1
+        for i in range(0, len(G)):
+            # przy zalozeniu ze nie ma krawedzi o wadze 0, 0 traktuje jako brak krawedzi
+            if visited[i] == 0:
+                if G[u][i] != 0:
+                    if (dist[i] > (dist[u] + G[u][i])):
+                        dist[i] = dist[u] + G[u][i]
+                        prev[i] = u
+                        H.put((dist[i], i))
+        tmp = prev[stop]
+        kolejnosc = []
+        kolejnosc.append(stop)
+        kolejnosc.append(tmp)
+        for i in range(2, len(prev)):
+            tmp = prev[tmp]
+            if tmp == -1:
+                break
+            kolejnosc.append(tmp)
+    # zwracane przez pierwotny algortym
+    # print(dist)
+    # print(prev)
+    # kolejnosc.reverse()
+    # return kolejnosc
+    kolejnosc.reverse()
+    return kolejnosc  # , dist[stop]
+
+
+# print("Przykład 1")
+# G = [
+#     [0, 2, 1, 7, 0, 3],
+#     [2, 0, 0, 6, 4, 9],
+#     [1, 0, 0, 3, 0, 0],
+#     [7, 6, 3, 0, 1, 0],
+#     [0, 4, 0, 1, 0, 0],
+#     [3, 9, 0, 0, 0, 0]
+# ]
+# print(Dijkstra(5, 2, G))
+# G1 = [
+#     [0, 1, 0, 0, 4, 8, 0, 0],
+#     [0, 0, 2, 0, 0, 6, 6, 0],
+#     [0, 0, 0, 1, 0, 0, 2, 0],
+#     [0, 0, 0, 0, 0, 0, 1, 4],
+#     [0, 0, 0, 0, 0, 5, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 1, 0, 1],
+#     [0, 0, 0, 0, 0, 0, 0, 0]
+# ]
+# print("Przykład 2")
+# print(Dijkstra(0, 7, G1))
+
+# g2 = [
+#     [0, 5, 7, 4],
+#     [5, 0, 1, 2],
+#     [7, 1, 0, 0],
+#     [4, 2, 0, 0]
+# ]
+# print("Przykład 3")
+# print(Dijkstra(0, 2, g2))
+
+
+def convertToNumpyArray(text):
+    rows = text.split(",\n")
+    s = len(rows)  # size
+    array = np.empty((s, s), dtype=float)
+    for i in range(len(s)):
+        newRow = np.array(np.fromstring(rows[i][1: -1], dtype=float, sep=","))
+        array[i] = newRow
+    return array
--- mainWindow.py
+++ mainWindow.py
@@ -1,22 +1,23 @@
 import PySimpleGUI as sg
+import Dijkstra as dk
 
 sg.theme("Dark Amber")
 user_input_column = [
     [sg.FileBrowse(button_text="Wybierz plik z grafem", size=(35, None),
-                   file_types=("Text files", "*.txt"), k="graphBrowser", enable_events=True)],
-    [sg.Button(button_text="Wylicz najkrótszą ścieżkę", size=(35, None))],
+                   file_types=(("Text files", "*.txt"),), k="graphBrowser", enable_events=True)],
+    [sg.Button(button_text="Wylicz najkrótszą ścieżkę",
+               size=(35, None), k="findSortestPath")],
     [sg.HorizontalSeparator(pad=(None, (10, 7)))],
     [sg.Text("Informacje o pliku:", size=(35, None), justification="center")],
     [sg.Multiline(size=(38, 3), auto_refresh=True,
                   tooltip="ścieżka do pliku", k="path")],
     [sg.Multiline(size=(38, 7), horizontal_scroll=True,
-                  tooltip="ścieżka do pliku", k="content")],
+                  tooltip="zawartość pliku", k="content")],
 ]
 
 user_output_column = [
-    [sg.Text("Choose an image from list on left:")],
-    [sg.Text(size=(40, 1), key="-TOUT-")],
-    [sg.Image(key="-IMAGE-")],
+    [sg.Multiline(size=(38, 3), auto_refresh=True,
+                  tooltip="wynik obliczeń", k="outputContent")],
 ]
 
 layout = [
@@ -34,15 +35,17 @@ while True:
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
     if event == "graphBrowser":
-        # folder = values["-FOLDER-"]
-        # try:
-        #     file_list = os.listdir(folder)
-        # except:
-        #     file_list = []
         filePath = values["graphBrowser"]
         file = open(filePath, "r")
         fileContent = file.read()
         file.close
         window["content"].update(fileContent)
         window["path"].update(filePath)
+    if event == "findSortestPath":
+        try:
+            graph = dk.convertToNumpyArray(values["content"])
+            content = str(dk.Dijkstra(5, 2, graph))
+            window["outputContent"].update(content)
+        except:
+            window["outputContent"].update("")
 window.close()
